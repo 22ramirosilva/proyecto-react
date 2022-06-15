aac1baa7a7241890c00e1a5811004e65e923e6d0
Ramiro Silva <22ramiro.silva@gmail.com>
2022-06-15 12:49:15 -0300

    primer commit

---
+++ src/componentes/Body/Nav/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Nav.css'
+import logoNav from '../../../Materiales/logo.png'
+
+const Nav = () => {
+    return (
+        <nav>
+        <div id="logotipo">
+            <a href="#body"><img id="logoimg" src={logoNav} alt="logo"/></a>
+        </div>
+        <ul id="lista_nav">
+            <li><b><a className="link_nav" href="#nosotros">Nosotros</a></b></li>
+            <li><b><a className="link_nav" href="#noticias">Noticias</a></b></li>
+            <li><b><a className="link_nav" href="">Ayuda</a></b></li>
+        </ul>
+
+        <button id="btn_ingresar">INGRESAR</button>
+    </nav>
+    )
+}
+
+export default Nav
